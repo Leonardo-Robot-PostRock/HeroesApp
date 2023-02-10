2598ed288e1fff1156da19bf34bf64c481f7a4bb
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-10 05:54:38 -0300

    navbar responsive

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -8,31 +8,42 @@ export const Navbar = () => {
       replace: true
     });
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2 fixed-top mb-30">
       <Link className="navbar-brand" to="/">
-        Asociaciones
+        <img src="../../../assets/dc-marvel.png" alt="dc y marvel" className='logo'/>
       </Link>
-
-      <div className="navbar-collapse">
+      <div className='d-flex'>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div className="collapse navbar-collapse" id="navbarNav">
         <div className="navbar-nav">
-          <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''} `} to="/marvel">
+          <NavLink className={({ isActive }) => `nav-link  text-end ${isActive ? 'active' : ''} `} to="/marvel">
             Marvel
           </NavLink>
 
-          <NavLink className={({ isActive }) => `nav-link ${isActive ? 'active' : ''} `} to="/dc">
+          <NavLink className={({ isActive }) => `nav-link text-end ${isActive ? 'active' : ''} `} to="/dc">
             DC
           </NavLink>
         </div>
-      </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-        <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Leonardo</span>
+        <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+          <ul className="navbar-nav ml-auto">
+            <span className="nav-item nav-link text-primary text-end">Leonardo</span>
 
-          <button className="nav-item nav-link btn" onClick={onLogout}>
-            Logout
-          </button>
-        </ul>
+            <button className="nav-item nav-link btn text-end" onClick={onLogout}>
+              Logout
+            </button>
+          </ul>
+        </div>
       </div>
     </nav>
   );
