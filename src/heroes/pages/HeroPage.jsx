4780292966741a622285d1cc5dea814c8f923b00
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-10 01:35:29 -0300

    Hero Page

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
-import { Navigate, useParams } from 'react-router-dom'
+import React from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { getHeroesById } from '../helpers';
 
 export const HeroPage = () => {
-
-  const {id} = useParams();
+  const { id } = useParams();
 
   const hero = getHeroesById(id);
 
-  if(!hero){
-    return <Navigate to="/marvel"/>
+  const navigate = useNavigate();
+
+  const onNavigateBack = () => {
+    if (hero.publisher === 'DC Comics') navigate('/dc');
+    else navigate('/marvel');
+  };
+
+  if (!hero) {
+    return <Navigate to="/marvel" />;
   }
-  
+
   return (
-    <h1>Hero</h1>
-  )
-}
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={`/assets/heroes/${id}.jpg`} alt={hero.superhero} className="img-thumbnail" />
+      </div>
+
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego: </b>
+            {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher: </b>
+            {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance: </b>
+            {hero.first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+          Regresar
+        </button>
+      </div>
+    </div>
+  );
+};
