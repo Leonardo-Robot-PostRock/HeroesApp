7433bfb52c983cbd98b6f5cae74f1febb80178ea
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-10 00:23:06 -0300

    import CharacterByHero, Link y parrafo

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { CharactersByHero } from './';
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
@@ -14,7 +16,12 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
             <div className="card-body">
               <h5 className="card-title">{superhero}</h5>
               <p className="card-text">{alter_ego}</p>
-              <p>{characters}</p>
+              <CharactersByHero alter_ego={alter_ego} characters={characters} />
+              <p className="card-text">
+                <small className="text-muted">{first_appearance}</small>
+              </p>
+
+              <Link to={`/hero/${id}`}>Mas..</Link>
             </div>
           </div>
         </div>
