42148a47a7e8f4bd5b9bca3b89dd9e0f84ca0001
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-10 00:42:46 -0300

    Hero Page

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { getHeroesById } from '../helpers';
 
 export const HeroPage = () => {
+
+  const {id} = useParams();
+
+  const hero = getHeroesById(id);
+
+  if(!hero){
+    return <Navigate to="/marvel"/>
+  }
+  
   return (
     <h1>Hero</h1>
   )
