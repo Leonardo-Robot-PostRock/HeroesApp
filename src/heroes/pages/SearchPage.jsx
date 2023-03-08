8bdc3194db320a6e4068bbad2408b1192d2493af
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-08 03:49:48 -0300

    showSearch showError

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -16,13 +16,16 @@ export const SearchPage = () => {
 
   console.log(heroes);
 
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
 
     navigate(`?q=${searchText}`);
   };
@@ -51,8 +54,13 @@ export const SearchPage = () => {
         <div className="col-12 col-md-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">
+
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? '' : 'none' }}>
+            Search a hero
+          </div>
+          <div className="alert alert-danger animate__animated animate__shakeX" style={{ display: showError ? '' : 'none' }}>
             No hero with <b>{q}</b>
           </div>
 
